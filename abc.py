db0be481bef776e0f6f31ad000cfcbbead29ee24
nogood4all <92sale@live.com>
2016-05-03 02:05:12 +0200

    Komentari
    Dodati komentari

--- abc.py
+++ abc.py
@@ -1,11 +1,13 @@
 from constraint import *
-
+#Trazimo takve ABC promenljive da ABC/A+B+C da ima ostatak pri deljenju 0
 def main():
     problem = Problem()
     problem.addVariables("abc", range(1,10))
+	#primer dodavanja nekih ogranicenja
     problem.addConstraint(lambda a,b,c: a!=b)
     problem.addConstraint(lambda a,b,c: a>c)
     problem.addConstraint(lambda a,b,c: c!=b)
+	#pronalazenje svih kombinacija koje postuju zadate uslove
     problem.getSolutions()
     minvalue = 999/(9*3)
     minsolution = {}
@@ -13,11 +15,14 @@ def main():
         a = solution["a"]
         b = solution["b"]
         c = solution["c"]
+		#Realno resenje
         value = (a*100+b*10+c)*1.0/(a+b+c)*1.0
-	value2 = (a*100+b*10+c)/(a+b+c)
+		#Celobrojno resenje
+		value2 = (a*100+b*10+c)/(a+b+c)
+		#Provera da li je ostatak 0
         if value < minvalue and value - value2 == 0 :
             minsolution = solution
-	    minvalue = value2
+			minvalue = value2
     print minvalue
     print minsolution
 
--- armstrong.py
+++ armstrong.py
@@ -2,14 +2,19 @@ from constraint import *
 
 def main():
     problem = Problem()
-    problem.addVariables("abc", range(1,10)) 
-#    problem.addConstraint(lambda a,b,c: a!=1)
+	#za potpuno ilustrovanje bi protrebno da promenljive, x y z, imaju mogucnost da uzmu vrednost 0
+	#ali to nije moguce zbog deljenja nulom, python prevodilac ne dozvoljava da postoji mogucnost deljenja nulom
+	#iako se obezbedimo da se taj slucaj nikad nece desiti
+    problem.addVariables("xyz", range(1,10)) 
+	#primer dodavanja ogranicenja na algoritam
+	#problem.addConstraint(lambda x,y,z: a!=1)
     problem.getSolutions()
     for solution in problem.getSolutions():
-        a = solution["a"]
-        b = solution["b"]
-        c = solution["c"]
-        value = (100*a+10*b+c)*1.0/(a**3+b**3+c**3)*1.0
+        x = solution["x"]
+        y = solution["y"]
+        z = solution["z"]
+		#XYZ = X^3+Y^3+Z^3
+        value = (100*x+10*y+z)*1.0/(x**3+y**3+z**3)*1.0
         if value == 1.0:
             print solution 
 
